4b3d45eb7e673dacb4fcd9a298971a50d0ebb062
Rafcin <Rafcin.s@gmail.com>
2019-02-08 04:19:11 +0000

    Fixed set_pks problem

--- modules/5-install_pkgs/5-install_pkgs.sh
+++ modules/5-install_pkgs/5-install_pkgs.sh
@@ -228,6 +228,7 @@ set_pkgs()
     "nano" "It is an easy-to-use text editor" $(pkgs_is_enabled "nano") \
     "htop" "Interactive processes viewer" $(pkgs_is_enabled "htop") \
     "iftop" "Network traffic viewer" $(pkgs_is_enabled "iftop") \
+    "vs_oss" "Adds Visual Studio code to the Jetson"
     "ZED" "Install ZED driver version:$INSTALL_ZED_VERSION" $(pkgs_is_enabled "ZED") 3>&1 1>&2 2>&3)
      
     exitstatus=$?
