e6c87359d77a8b6bb6bfbd4329f2d5807abbae35
Raffaello Bonghi <raffaello@rnext.it>
2019-05-13 08:18:43 +0200

    Update 5-install_pkgs.sh
    Update ZED SDK with version 2.8

--- modules/5-install_pkgs/5-install_pkgs.sh
+++ modules/5-install_pkgs/5-install_pkgs.sh
@@ -43,7 +43,7 @@ MODULE_OPTIONS=("RUN" "STOP")
 MODULE_SUBMENU=("Add new packages:set_pkgs")
 
 #Will add check for Zed version status later.
-INSTALL_ZED_VERSION="2.7"
+INSTALL_ZED_VERSION="2.8"
 
 pkgs_is_enabled()
 {
